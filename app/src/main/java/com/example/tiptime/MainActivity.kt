1d3f1d80a81704f04620f272b312eb2bd4ff9200
rsgur <rafaelgs23@educastur.es>
2023-11-01 15:48:10 +0100

    Lógica programa terminada. Operaciones finalizada.

--- app/src/main/java/com/example/tiptime/MainActivity.kt
+++ app/src/main/java/com/example/tiptime/MainActivity.kt
@@ -2,10 +2,35 @@ package com.example.tiptime
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.example.tiptime.databinding.ActivityMainBinding
+import java.text.NumberFormat
+import java.text.NumberFormat.*
 
 class MainActivity : AppCompatActivity() {
+    lateinit  var binding: ActivityMainBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        binding.boton1.setOnClickListener { calculateTip() }
+    }
+    fun calculateTip(){
+
+        val costeServicio = binding.costeServicio.text.toString().toDouble()
+        val porcentaje = when(binding.grupoBotones.checkedRadioButtonId){
+            R.id.PrimerBoton -> 0.20
+            R.id.SegundoBoton -> 0.18
+            else -> 0.15
+        }
+
+        var propina = porcentaje*costeServicio
+        if(binding.redondear.isChecked) {
+            propina = kotlin.math.ceil(propina)
+        }
+        binding.propina.text = getString(R.string.propina_resultante,
+            getCurrencyInstance().format(propina))
+
+
     }
 }
